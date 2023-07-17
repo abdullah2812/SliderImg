be3bd90501ed0de9cae9cd9aa86e71525dd758b4
abdullah2812 <irianda546@gmail.com>
2023-07-17 19:00:04 +0700

    Meet7

--- android/app/src/screens/ShowProductScreen.js
+++ android/app/src/screens/ShowProductScreen.js
@@ -1,7 +1,7 @@
 import {View, Text} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import realm from '../../store/realm';
-import {Icon} from 'react-native-elements';
+import {Icon, CheckBox} from 'react-native-elements';
 import {
   View,
   Text,
@@ -12,11 +12,12 @@ import {
 } from 'react-native';
 import {categoryList} from '../data/Data';
 import {MediaComponent} from '../components/MediaComponent';
-import { Linking } from 'react-native';
+import {Linking} from 'react-native';
 import {
   widthPercentageToDP as wp,
-  heightPercentageToDP as hp
+  heightPercentageToDP as hp,
 } from 'react-native-responsive-screen-hooks';
+import {UserState} from 'realm';
 
 const styles = StyleSheet.create({
   mainContainer: {
@@ -42,7 +43,7 @@ const styles = StyleSheet.create({
   },
   image: {
     width: wp('25%'),
-    height:  hp('25%'),
+    height: hp('25%'),
   },
   textContainer: {
     flex: 1,
@@ -64,25 +65,33 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
     justifyContent: 'center',
-    alignItems: 'center'
-}, box: {
+    alignItems: 'center',
+  },
+  box: {
     width: '80%',
     backgroundColor: 'white',
     padding: 16,
     borderWidth: 1,
     borderRadius: 10,
-    alignItems: 'center'
-}, cancel: {
+    alignItems: 'center',
+  },
+  cancel: {
     padding: 8,
     position: 'absolute',
     right: 8,
-    top: 8
-},
-sellerText: {
+    top: 8,
+  },
+  sellerText: {
     marginBottom: 8,
-    marginTop: 32
-}
+    marginTop: 32,
+  },
+  checkBox: {
+    position: 'absolute',
+    right: 0,
+  },
 });
+
+const [isRemove, setIsRemove] = UserState(false);
 
 const [data, setData] = useState([]);
 
@@ -90,7 +99,7 @@ const [isBuy, setIsBuy] = useState(false);
 const [contact, setContact] = useState({
   phoneNumber: '',
   instagram: '',
-  facebook: ''
+  facebook: '',
 });
 
 const buyProduct = (whatsapp, instagramId, facebookId) => {
@@ -105,21 +114,40 @@ const buyProduct = (whatsapp, instagramId, facebookId) => {
 const collectData = () => {
   const allData = realm.objects('Product').filtered('category = ${id}'); //object => untuk mengakses database Product
   setData(allData);
+
+  const newData = allData.map(item => {
+    item.checkedStatus = false;
+    return item;
+  });
+  setData(newData);
+  console.log(newData);
 };
 
-
-const onClickMedia = (type) => {
+const onClickMedia = type => {
   if (type === 'whatsapp') {
-    Linking.openURL('https://wa.me/${contact.numberPhone}')
-  }else if (type === 'instagram'){
-    Linking.openURL('https://www.instagram.com/${contact.instagram}')
-  }else if (type == 'facebook'){
-    Linking.openURL('https://m.me/${contact.facebook}')
+    Linking.openURL('https://wa.me/${contact.numberPhone}');
+  } else if (type === 'instagram') {
+    Linking.openURL('https://www.instagram.com/${contact.instagram}');
+  } else if (type == 'facebook') {
+    Linking.openURL('https://m.me/${contact.facebook}');
   }
 };
 
 const {navigation} = props;
 
+const ShowProductScreen = props => {
+  const {route} = props;
+  const category = route.params.keyExtractor;
+};
+
+const setCheckBox = (id, status) => {
+  const newData = data.map((item) => {
+      if (item.id === id) {
+      }   item.checkedStatus = !status;
+      return item;
+  }); 
+  setData(newData)
+};
 
 useEffect(() => {
   const productPage = navigation.addListener('focus', () => {});
@@ -134,16 +162,22 @@ useEffect(() => {
     keyExtractor={item => item.id}
     renderItem={({item}) => {
       return (
-        <TouchableOpacity 
-        style={styles.itemButton}
-        onPress={()=>navigation.navigate('EditProduct',{ //diambil dari main navigator yang telah kita daftarkan
-          idProduct:item.id
-        })}
-        >
+        <TouchableOpacity
+          style={styles.itemButton}
+          onPress={() =>
+            navigation.navigate('EditProduct', {
+              //diambil dari main navigator yang telah kita daftarkan
+              idProduct: item.id,
+            })
+          }
+          onLongPress={() => setIsRemove(true)}>
           <View style={styles.productContainer}>
-            <TouchableOpacity onPress={()=> navigation.navigate('ImageZoom',{imagePath:item.imagePath})}>
+            <TouchableOpacity
+              onPress={() =>
+                navigation.navigate('ImageZoom', {imagePath: item.imagePath})
+              }>
               <Image style={styles.image} source={{uri: item.imagePath}} />
-            </TouchableOpacity>     
+            </TouchableOpacity>
             <View style={styles.textContainer}>
               <Text style={styles.title}>{item.productName}</Text>
               <Text style={styles.text}>{item.description}</Text>
@@ -167,37 +201,43 @@ useEffect(() => {
         </TouchableOpacity>
         <Text style={[styles.sellerText, styles.title]}>
           Contact the seller through this media :
-        </Text>{
-           contact.phoneNumber !== '' ?
-           <MediaComponent
-               source={require('../../assets/images/whatsapp.png')}
-               value={contact.phoneNumber}
-               onPress={() => onClickMedia('whatsapp')}
-           />
-           :
-           contact.facebook !== '' ?
-           <MediaComponent
-               source={require('../../assets/images/facebook.png')}
-               value={contact.facebook}
-               onPress={() => onClickMedia('facebook')}
-           /> :
-           contact.instagram !== '' ?
-           <MediaComponent
-               source={require('../../assets/images/instagram.png')}
-               value={contact.instagram}
-               onPress={() => onClickMedia('instagram')}
-           />
-          :
-          null
-        }
+        </Text>
+        {contact.phoneNumber !== '' ? (
+          <MediaComponent
+            source={require('../../assets/images/whatsapp.png')}
+            value={contact.phoneNumber}
+            onPress={() => onClickMedia('whatsapp')}
+          />
+        ) : contact.facebook !== '' ? (
+          <MediaComponent
+            source={require('../../assets/images/facebook.png')}
+            value={contact.facebook}
+            onPress={() => onClickMedia('facebook')}
+          />
+        ) : contact.instagram !== '' ? (
+          <MediaComponent
+            source={require('../../assets/images/instagram.png')}
+            value={contact.instagram}
+            onPress={() => onClickMedia('instagram')}
+          />
+        ) : null}
       </View>
     </View>
   ) : null}
-</View>;
 
-const ShowProductScreen = props => {
-  const {route} = props;
-  const category = route.params.keyExtractor;
-};
+  {isRemove ? (
+    <CheckBox size={30} containerStyle={styles.checkBox} 
+      onPress={()=> setCheckBox(item.id, item.checkedStatus)}
+      checked={item.checkedStatus}
+    />
+  ) : (
+    <TouchableOpacity
+      onPress={() =>
+        buyProduct(item.phoneNumber, item.instagram.item.facebook)
+      }>
+      <Icon name="shoppingcart" type="antdesign" size={30} />
+    </TouchableOpacity>
+  )}
+</View>;
 
 export default ShowProductScreen;
